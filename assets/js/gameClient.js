17a1f3c1c5d9456429757290b5b18e0dc2f161ef
aviratb <avirat.8151@gmail.com>
2021-03-27 22:01:59 +0530

    make it better

--- assets/js/gameClient.js
+++ assets/js/gameClient.js
@@ -1,28 +1,39 @@
 let boxes = document.querySelectorAll('.grid-container div');
-let colors = ['blue', 'red', 'pink', 'yellow', 'white', 'green', 'black', 'brown', 'yellow', 'green', 'black', 'brown', 'red', 'pink', 'blue', 'white'];
-let prevColor, currentColor, j = 0, prevBoxPosition, currentBoxPosition, count = 0;
+let colors = ['blue', 'red', 'brown', 'yellow', 'white', 'green', 'black', 'pink',
+    'yellow', 'green', 'red', 'brown', 'black', 'pink', 'blue', 'white'];
+let prevColor, currentColor, prevBoxPosition, currentBoxPosition;
+let count = 0, j = 0;
+let flag = true;
 
 for (let i = 0; i < boxes.length; i++) {
     boxes[i].onclick = function (event) {
         event.preventDefault();
-        this.style.background = colors[i];
-        if (j % 2 == 0) {
-            prevColor = colors[i];
-            prevBoxPosition = i;
+        if (flag) {
+            flag = false;
+            this.style.background = colors[i];
+
+            if (j % 2 == 0) {
+                prevColor = colors[i];
+                prevBoxPosition = i;
+                flag = true;
+            }
+
+            if (j % 2 == 1) {
+                currentColor = colors[i];
+                currentBoxPosition = i;
+                setTimeout(() => {
+                    if (prevColor != currentColor) {
+                        boxes[prevBoxPosition].style.background = "grey";
+                        boxes[currentBoxPosition].style.background = "grey";
+                    }
+                    flag = true;
+                }, 800);
+                count++;
+                document.getElementById('count').innerHTML = `${count}`
+            }
+
+            j++;
         }
-        if (j % 2 == 1) {
-            currentColor = colors[i];
-            currentBoxPosition = i;
-            setTimeout(() => {
-                if (prevColor != currentColor) {
-                    boxes[prevBoxPosition].style.background = "grey";
-                    boxes[currentBoxPosition].style.background = "grey";
-                }
-            }, 800);
-            count++;
-            document.getElementById('count').innerHTML = `${count}`
-        }
-        j++;
     }
 }
 
